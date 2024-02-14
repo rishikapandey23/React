e7387d9d2a12769677dc4fbb562b3ec4a8b27888
rishikapandey23 <rishikapandey02@gmail.com>
2024-02-14 14:11:20 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,32 @@
+
+/* <div id="parent">
+<div id="child">
+    <h1>I am h1 tag</h1>
+    <h2>I am h1 tag</h2>
+
+</div>
+<div id="child2">
+    <h1>I am h1 tag</h1>
+    <h2>I am h1 tag</h2>
+
+</div>
+</div> */
+
+const parent= React.createElement("div", {id:"parent"},[React.createElement("div", {id:"child"},[React.createElement("h1", {},"I am h1 tag"),
+React.createElement("h2", {},"I am h2 tag")]
+),React.createElement("div", {id:"child2 "},[React.createElement("h1", {},"I am h1 tag"),
+React.createElement("h2", {},"I am h2 tag")]
+)]
+);
+
+//jsx 
+
+
+
+
+
+
+
+// const heading= React.createElement("h1", {id: "heading"}, "Hello from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
